86da29849552a0b047614c8c0545d4cb04ade622
73745543 <plebs89@gmail.com>
2022-02-12 21:01:32 +0100

    Aktualizacja projektu Library do wersji 1.0

--- Library/src/pl/javastart/library/app/LibraryApp.java
+++ Library/src/pl/javastart/library/app/LibraryApp.java
@@ -1,9 +1,10 @@
 package pl.javastart.library.app;
 
 class LibraryApp {
+    private static final String APP_NAME = "Biblioteka v1.0";
+
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+        System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
     }
