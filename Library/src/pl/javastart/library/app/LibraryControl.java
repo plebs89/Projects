910ce8b4dddb98be56b596d512f189dd737c5bd8
73745543 <plebs89@gmail.com>
2022-02-28 19:17:35 +0100

    Aktualizacja projektu Library do wersji 1.1

--- Library/src/pl/javastart/library/app/LibraryControl.java
+++ Library/src/pl/javastart/library/app/LibraryControl.java
@@ -3,34 +3,38 @@ package pl.javastart.library.app;
 import pl.javastart.library.io.DataReader;
 import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
+import pl.javastart.library.model.Magazine;
 
-class LibraryControl {
-    //zmienne do kontrolowania programu
+public class LibraryControl {
+    // stałe do kontrolowania programu
     private static final int EXIT = 0;
     private static final int ADD_BOOK = 1;
-    private static final int PRINT_BOOKS = 2;
+    private static final int ADD_MAGAZINE = 2;
+    private static final int PRINT_BOOKS = 3;
+    private static final int PRINT_MAGAZINES = 4;
 
-    //zmienna do komunikacji z użytkownikiem
     private DataReader dataReader = new DataReader();
 
-    //"biblioteka| przechowująca dane
     private Library library = new Library();
 
-    /*
-    *Główna metoda programu, która pozwala na wybór opcji i interakcję
-     */
-    public void controlLoop(){
+    public void controlLoop() {
         int option;
 
         do {
             printOptions();
             option = dataReader.getInt();
-            switch (option){
+            switch (option) {
                 case ADD_BOOK:
                     addBook();
                     break;
+                case ADD_MAGAZINE:
+                    addMagazine();
+                    break;
                 case PRINT_BOOKS:
                     printBooks();
+                    break;
+                case PRINT_MAGAZINES:
+                    printMagazines();
                     break;
                 case EXIT:
                     exit();
@@ -38,28 +42,39 @@ class LibraryControl {
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
             }
-        }while (option != EXIT);
+        } while (option != EXIT);
     }
 
-    private void printOptions(){
+    private void printOptions() {
         System.out.println("Wybierz opcję: ");
         System.out.println(EXIT + " - wyjście z programu");
         System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(PRINT_BOOKS + " - wyświetl dodane książki");
+        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
+        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
+        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
     }
 
-    private void addBook(){
+    private void addBook() {
         Book book = dataReader.readAndCreateBook();
         library.addBook(book);
     }
 
-    private void printBooks(){
+    private void printBooks() {
         library.printBooks();
     }
 
-    private void exit(){
+    private void addMagazine() {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        library.addMagazine(magazine);
+    }
+
+    private void printMagazines() {
+        library.printMagazines();
+    }
+
+    private void exit() {
         System.out.println("Koniec programu, papa!");
-        //zamykamy strumień wejścia
+        // zamykamy strumień wejścia
         dataReader.close();
     }
 }
--- Library/src/pl/javastart/library/io/DataReader.java
+++ Library/src/pl/javastart/library/io/DataReader.java
@@ -1,17 +1,18 @@
 package pl.javastart.library.io;
 
 import pl.javastart.library.model.Book;
+import pl.javastart.library.model.Magazine;
 
 import java.util.Scanner;
 
 public class DataReader {
-    private final Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
 
     public void close() {
         sc.close();
     }
 
-    public int getInt(){
+    public int getInt() {
         int number = sc.nextInt();
         sc.nextLine();
         return number;
@@ -27,12 +28,27 @@ public class DataReader {
         System.out.println("ISBN: ");
         String isbn = sc.nextLine();
         System.out.println("Rok wydania: ");
-        int releaseDate = sc.nextInt();
-        sc.nextLine();
+        int releaseDate = getInt();
         System.out.println("Ilość stron: ");
-        int pages = sc.nextInt();
-        sc.nextLine();
+        int pages = getInt();
 
         return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+    public Magazine readAndCreateMagazine() {
+        System.out.println("Tytuł: ");
+        String title = sc.nextLine();
+        System.out.println("Wydawnictwo: ");
+        String publisher = sc.nextLine();
+        System.out.println("Język: ");
+        String language = sc.nextLine();
+        System.out.println("Rok wydania: ");
+        int year = getInt();
+        System.out.println("Miesiąc: ");
+        int month = getInt();
+        System.out.println("Dzień: ");
+        int day = getInt();
+
+        return new Magazine(title, publisher, language, year, month, day);
     }
 }
