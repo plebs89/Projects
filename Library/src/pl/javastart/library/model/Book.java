65e7e38b7270b7379b566840688a728bb2c892cd
73745543 <plebs89@gmail.com>
2022-03-01 19:01:55 +0100

    Aktualizacja projektu Library do wersji 1.2

--- Library/src/pl/javastart/library/model/Book.java
+++ Library/src/pl/javastart/library/model/Book.java
@@ -7,11 +7,8 @@ public class Book extends Publication {
     private String isbn;
 
     // Konstruktory
-    public Book(String title, String author, int year, int pages, String publisher,
-                String isbn) {
-        setTitle(title);
-        setPublisher(publisher);
-        setYear(year);
+    public Book(String title, String author, int year, int pages, String publisher, String isbn) {
+        super(title, publisher, year);
         this.pages = pages;
         this.author = author;
         this.isbn = isbn;
