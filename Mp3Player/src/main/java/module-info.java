8f54b7084c26d0ed9bfa06b562cecd353c3ed4b0
73745543 <plebs89@gmail.com>
2022-06-20 12:01:08 +0200

    Mp3Player 1.4

---
+++ Mp3Player/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Mp3Player {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports pl.javastart.mp3player.main to javafx.graphics;
+    opens pl.javastart.mp3player.controller to javafx.fxml;
+}
