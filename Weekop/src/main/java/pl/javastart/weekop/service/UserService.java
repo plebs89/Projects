3a6c4ee9eb118e1b75833bad337b3e0684ec70d7
plebs89 <plebs89@gmail.com>
2022-11-28 20:44:52 +0100

    Weekop 2.5

--- Weekop/src/main/java/pl/javastart/weekop/service/UserService.java
+++ Weekop/src/main/java/pl/javastart/weekop/service/UserService.java
@@ -1,5 +1,9 @@
 package pl.javastart.weekop.service;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import pl.javastart.weekop.dao.DAOFactory;
 import pl.javastart.weekop.dao.UserDAO;
 import pl.javastart.weekop.model.User;
@@ -8,12 +12,25 @@ public class UserService {
     public void addUser(String username, String email, String password) {
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password);
+        String md5Pass = encryptPassword(password);
+        user.setPassword(md5Pass);
         user.setEmail(email);
         user.setActive(true);
         DAOFactory factory = DAOFactory.getDAOFactory();
         UserDAO userDao = factory.getUserDAO();
         userDao.create(user);
+    }
+
+    private String encryptPassword(String password) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        digest.update(password.getBytes());
+        String md5Password = new BigInteger(1, digest.digest()).toString(16);
+        return md5Password;
     }
 
     public User getUserById(long userId) {
