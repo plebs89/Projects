0e3b897f9cbe1d86084f77629c588a345745edab
plebs89 <plebs89@gmail.com>
2024-07-11 22:28:11 +0200

    Foodie 1.1 main page

---
+++ Foodie/src/main/java/com/example/foodie/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.foodie;
+
+import com.example.foodie.item.Item;
+import com.example.foodie.item.ItemRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final ItemRepository itemRepository;
+
+    public HomeController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "index";
+    }
+}
