7a4c0f8788100bb9c38b084b963767ff089f3e13
plebs89 <plebs89@gmail.com>
2024-07-30 21:14:05 +0200

    Equipy 2.1 equipment assignment

--- Equipy/src/main/java/pl/javastart/equipy/components/user/UserRepository.java
+++ Equipy/src/main/java/pl/javastart/equipy/components/user/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPesel(String pesel);
     List<User> findAllByLastNameContainingIgnoreCase(String lastName);
 }
